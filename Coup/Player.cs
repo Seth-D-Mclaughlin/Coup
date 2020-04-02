76b94de7e9b128f6ce5bcce925a2808e9bf8f9f7
Seth McLaughlin <Seth.d.mclaughlin@gmail.com>
2020-04-02 14:14:15 -0400

    Coup

---
+++ Coup/Player.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coup
+{
+    public class Bank
+    {
+        public int balance = 50;
+
+        public Bank() { }
+    }
+    public class Deck
+    {
+        public List<string> influences = new List<string> { "Duke", "Duke", "Duke", "Contessa", "Contessa", "Contessa", "Assassin", "Assassin", "Assassin", "Ambassador", "Ambassador", "Ambassador", "Captain", "Captain", "Captain" };
+        public Deck() { }
+
+        //Random rnd = new Random();
+        //int card = rnd.Next(14);
+
+        public void Draw(Player player)
+        {
+            int deckLength = influences.Count(); //Gets length of deck
+            Random number = new Random();
+            int num = number.Next(0, deckLength);
+            string newCard = influences[num];
+            influences.RemoveAt(num);
+
+            player.PlayerHand.Add(newCard);
+        }
+        public void Deal(List<Player> Players)
+        {
+            foreach (Player i in Players)
+            {
+                Draw(i);
+                Draw(i);
+                i.Coins += 2;
+            }
+        }
+        public List<Player> PutActivePlayersInList()
+        {
+            Player Player1 = new Player(0) { };
+            Player Player2 = new Player(0) { };
+            Player Player3 = new Player(0) { };
+            Player Player4 = new Player(0) { };
+            Player Player5 = new Player(0) { };
+            Player Player6 = new Player(0) { };
+            List<Player> inactiveplayers = new List<Player> { Player1, Player2, Player3, Player4, Player5, Player6 };
+
+
+            Console.WriteLine("How many players will there be between 2-6");
+            int numOfPlayers = Int32.Parse(Console.ReadLine());
+            List<Player> ActivelistOfPlayers = new List<Player>() { };
+            for (int i = 0; i <= numOfPlayers; i++)
+            {
+                ActivelistOfPlayers.Add(inactiveplayers[i]);
+            }
+            return ActivelistOfPlayers;
+        }
+    }
+    public class Player
+    {
+        //Properties
+        public int Coins { get; set; }
+        public List<string> PlayerHand = new List<string>() { };
+        //Constructor
+        public Player() { }
+        public Player(int coins)
+        {
+            Coins = coins;
+
+        }
+        // Methods
+        public void Steal(Player theif, Player victim)
+        {
+            theif.Coins += 2;
+            victim.Coins -= 2;
+        }
+        public void Assassinate(Player Killer, Player victim, Bank Account)
+        {
+            if (Killer.Coins >= 3)
+            {
+                Killer.Coins -= 3;
+                Account.balance += 3;
+                if (victim.PlayerHand.Count >= 2)
+                {
+                    Console.WriteLine("Pass to the person you are killing.\n Press any key when victim is ready.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Console.WriteLine($"0. {victim.PlayerHand[0]} \n 1. {victim.PlayerHand[1]}");
+                    string victimChoice = Console.ReadLine();
+                    int victimNum = Int32.Parse(victimChoice);
+                    victim.PlayerHand.RemoveAt(victimNum);
+                }
+                else
+                {
+                    victim.PlayerHand.Clear();
+                }
+            }
+
+        }
+        public void Exchange(Player Exchanger, Deck CourtDeck)
+        {
+            CourtDeck.Draw(Exchanger);
+            CourtDeck.Draw(Exchanger);
+            if (Exchanger.PlayerHand.Count == 3)                    //1 cap  2 ass 3 ass 4 amas
+            {
+                Console.WriteLine("Choose 1 card to keep.");
+                ShowHand(Exchanger);
+                int input = Int32.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case 0:
+                        string keeper0 = Exchanger.PlayerHand[0];
+                        CourtDeck.influences.Add(Exchanger.PlayerHand[1]);
+                        CourtDeck.influences.Add(Exchanger.PlayerHand[2]);
+                        Exchanger.PlayerHand.Clear();
+                        Exchanger.PlayerHand.Add(keeper0);
+                        break;
+                    case 1:
+                        string keeper1 = Exchanger.PlayerHand[1];
+                        CourtDeck.influences.Add(Exchanger.PlayerHand[0]);
+                        CourtDeck.influences.Add(Exchanger.PlayerHand[2]);
+                        Exchanger.PlayerHand.Clear();
+                        Exchanger.PlayerHand.Add(keeper1);
+                        break;
+                    case 2:
+                        string keeper2 = Exchanger.PlayerHand[2];
+                        CourtDeck.influences.Add(Exchanger.PlayerHand[0]);
+                        CourtDeck.influences.Add(Exchanger.PlayerHand[1]);
+                        Exchanger.PlayerHand.Clear();
+                        Exchanger.PlayerHand.Add(keeper2);
+                        break;
+                }
+            }
+            else if (Exchanger.PlayerHand.Count == 4)
+            {
+                Console.WriteLine("You may keep 2 cards. Enter first card name to keep.");
+                for (int i = 1; i <= Exchanger.PlayerHand.Count; i++)
+                {
+                    Console.WriteLine($"{i}. {Exchanger.PlayerHand[i]}");
+                }
+                string input = Console.ReadLine();
+                Console.WriteLine("Enter second card name to keep.");
+                string input2 = Console.ReadLine();
+
+                List<string> keepers = new List<string>() { };
+                for (int i = 1; i <= Exchanger.PlayerHand.Count; i++)
+                {
+
+                    if (input == Exchanger.PlayerHand[i])
+                    {
+                        string keeper = Exchanger.PlayerHand[i];
+                        keepers.Add(keeper);
+                    }
+                    else if (input2 == Exchanger.PlayerHand[i])
+                    {
+                        string keeper = Exchanger.PlayerHand[i];
+                        keepers.Add(keeper);
+                    }
+                    else
+                    {
+                        //Fat finger case
+                    }
+
+
+                }
+                for (int i = 1; i <= Exchanger.PlayerHand.Count; i++)
+                {
+                    if (keepers[i] != Exchanger.PlayerHand[i])
+                    {
+                        CourtDeck.influences.Add(Exchanger.PlayerHand[i]);
+                        Exchanger.PlayerHand.Remove(Exchanger.PlayerHand[i]);
+                    }
+                }
+
+            }
+
+
+
+        }
+        public void ShowHand(Player player)
+        {
+            for (int i = 0; i < player.PlayerHand.Count; i++)
+            {
+                Console.WriteLine($"{i}. {player.PlayerHand[i]}");
+            }
+            Console.WriteLine("Press any key to continue..");
+            Console.ReadKey();
+        }
+        public void Tax(Player Duke, Bank amount)
+        {
+            Duke.Coins += 3;
+            amount.balance -= 3;
+        }
+        public void Income(Player player, Bank amount)
+        {
+            player.Coins += 1;
+            amount.balance -= 1;
+        }
+        public void ForeignAid(Player player, Bank amount)
+        {
+            player.Coins += 2;
+            amount.balance -= 2;
+        }
+        public void Coup(Player killer, Player victim, Bank amount)
+        {
+            killer.Coins -= 7;
+            amount.balance += 7;
+
+
+            if (victim.PlayerHand.Count >= 2)
+            {
+                Console.WriteLine("Pass to the person you are killing.\n Press any key when victim is ready.");
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine($"0. {victim.PlayerHand[0]} \n 1. {victim.PlayerHand[1]}");
+                string victimChoice = Console.ReadLine();
+                int victimNum = Int32.Parse(victimChoice);
+                victim.PlayerHand.RemoveAt(victimNum);
+            }
+            else
+            {
+                victim.PlayerHand.Clear();
+            }
+        }
+
+
+
+
+    }
+}
+
---
+++ Coup/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coup
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Deck _CourtDeck = new Deck() { }; // creating the courtdeck
+            Bank CorruptCompany = new Bank() { };
+            //WelcomeMessage(CourtDeck); //Welcome 
+            List<Player> _playerList =  _CourtDeck.PutActivePlayersInList(); // gathering players
+            _CourtDeck.Deal(_playerList); //Dealing two cards to everyone
+            Turn(_playerList,CorruptCompany,_CourtDeck);
+
+
+        }
+
+        static Player Target(List<Player> playerList)
+        {
+            Console.WriteLine("Please select your target");
+            for (int i = 0; i < playerList.Count(); i++)
+            {
+                Console.WriteLine($"{i}) {playerList[i]}");
+            }
+            string userInput = Console.ReadLine();
+            int input = Int32.Parse(userInput);
+
+            switch (input)
+            {
+                case 0:
+                    {
+                        Player Target = playerList[input];
+                        return Target;
+
+                    }
+                case 1:
+                    {
+                        Player Target = playerList[input];
+                        return Target;
+
+                    }
+                case 2:
+                    {
+                        Player Target = playerList[input];
+                        return Target;
+
+                    }
+
+                case 3:
+                    {
+                        Player Target = playerList[input];
+                        return Target;
+                    }
+                case 4:
+                    {
+                        Player Target = playerList[input];
+                        return Target;
+                    }
+                case 5:
+                    {
+                        Player Target = playerList[input];
+                        return Target;
+                    }
+                case 6:
+                    {
+                        Player Target = playerList[input];
+                        return Target;
+                    }
+                default:
+                    {
+                        Player Dummy = new Player() { };
+                        return Dummy;
+                    }
+            }
+        }
+
+        static void Turn(List<Player> playerList, Bank corruptCompany, Deck courtDeck)
+        {
+            for (int i = 0; i < playerList.Count(); i++)
+            {
+                switch (i)
+                {
+                    case 0://player 1
+
+                        Console.WriteLine($"{playerList[i]}'s turn.");
+                        ActionPhase(corruptCompany, playerList[i], courtDeck, playerList);
+                        break;
+                    case 1:
+                        Console.WriteLine($"{playerList[i]}'s turn.");
+                        ActionPhase(corruptCompany, playerList[i], courtDeck, playerList);
+                        break;
+                    case 2:
+                        Console.WriteLine($"{playerList[i]}'s turn.");
+                        ActionPhase(corruptCompany, playerList[i], courtDeck, playerList);
+                        break;
+                    case 3:
+                        Console.WriteLine($"{playerList[i]}'s turn.");
+                        ActionPhase(corruptCompany, playerList[i], courtDeck, playerList);
+                        break;
+                    case 4:
+                        Console.WriteLine($"{playerList[i]}'s turn.");
+                        ActionPhase(corruptCompany, playerList[i], courtDeck, playerList);
+                        break;
+                    case 5:
+                        Console.WriteLine($"{playerList[i]}'s turn.");
+                        ActionPhase(corruptCompany, playerList[i], courtDeck, playerList);
+                        break;
+                    case 6:
+                        i = 0;
+                        break;
+                }
+            }
+        }
+
+
+        static void ActionPhase(Bank corruptCompany, Player player, Deck courtdeck, List<Player> playerList)
+        {
+
+            bool continueToRun = true;
+            while (continueToRun)
+            {
+
+                Console.Clear();
+                Console.WriteLine($"Enter the number of the option you'd like to select: \n" +
+                    "1) Choose to take 1 coin\n" +
+                    "2) Take 2 coins \n" +
+                    "3) Play a Duke Card \n" +
+                    "4) Play a Captain Card \n" +
+                    "5) Play an Ambassador Card\n" +
+                    "6) Play an Assassin Card.\n" +
+                    "7) Coup! \n" +
+                    "8) See your cards.");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        {
+                            //Show all
+                            player.Income(player, corruptCompany);
+
+                            break;
+                        }
+                    case "2":
+                        {
+                            //ForeignAid
+                            player.ForeignAid(player, corruptCompany);
+                            break;
+                        }
+                    case "3":
+                        {
+                            //Dukes option
+                            player.Tax(player, corruptCompany);
+                            break;
+                        }
+                    case "4":
+                        {
+                            //Captain
+                            player.Steal(player, Target(playerList)); //figure out how were going to decide to steal from
+                            break;
+                        }
+                    case "5":
+                        {
+                            player.Exchange(player, courtdeck);
+                            break;
+                        }
+                    case "6":
+                        {
+                            Player target = Target(playerList);
+                            player.Assassinate(player,target,corruptCompany);
+                            break;
+                        }
+                    case "7":
+                        {
+                            player.Coup(player, Target(playerList), corruptCompany);
+                            break;
+                        }
+                    case "8":
+                        {
+                            player.ShowHand(player);
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
+
+
